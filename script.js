64c6603f824e8ccf1c70f72c6f0e14f3f4d88515
Krishna Sharma <118767245+Krishna6438@users.noreply.github.com>
2024-04-21 23:08:35 +0530

    Added all the files

---
+++ script.js
@@ -0,0 +1,45 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute(
+        "class",
+        isOpen ? "ri-close-line" : "ri-menu-3-line"
+    );
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOptions = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+
+
+// service container
+ScrollReveal().reveal(".service__card", {
+    ...scrollRevealOptions,
+    interval: 500,
+});
+
+// price container
+ScrollReveal().reveal(".price__card", {
+    ...scrollRevealOptions,
+    interval: 1000,
+});
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
